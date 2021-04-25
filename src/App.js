b6a06f82c8c0b52cb3a543d25d5cedbfc17a387a
FARUKH KHAN <faf05.khan@gmail.com>
2021-04-25 15:19:15 +0530

    Table.js commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import{FormControl,Select,MenuItem, Card, CardContent} from '@material-ui/core';
 import InfoBox from './InfoBox';
 import Map from './Map';
 import './App.css';
+import Table from './Table';
 
 function App() {
   const [countries, setcountries] = useState([]);
   const [country, setcountry] = useState('Worldwide');
   const [countryInfo, setcountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(()=>{
     fetch('https://disease.sh/v3/covid-19/all').then((response)=> response.json()).then((data)=>{
@@ -23,6 +25,7 @@ function App() {
           value: country.countryInfo.iso2,
         }));
         setcountries(countries);
+        setTableData(data);
       })
     }
     getCountriesData();
@@ -50,7 +53,7 @@ function App() {
         <div className='app__header'>
           <h1>COVID-19 TRACKER</h1>
           <FormControl className='app__dropdown'>
-            <Select variant='outlined' onChange={onCountryChange} value={country }>
+            <Select variant='outlined' onChange={onCountryChange} value={country}>
             <MenuItem value='worldwide'>Worldwide</MenuItem>
             {countries.map((country)=>(
               <MenuItem value={country.value}>{country.name}</MenuItem>
@@ -69,6 +72,7 @@ function App() {
         <Card className='app__right'>
           <CardContent>
             <h3>Live Cases by Country</h3>
+            <Table countries={setTableData}/>
             <h3>Worldwide new cases</h3>
           </CardContent>
         </Card>
